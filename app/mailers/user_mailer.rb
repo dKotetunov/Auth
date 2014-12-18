9217f421e6d3a6e4aebc2e83e21ee2fb94da9ea1
dkotetunov <dkotetunov@gmail.com>
2014-12-18 23:50:15 +0200

    add mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,9 @@
 class UserMailer < ActionMailer::Base
-
+  default from: 'notifications@example.com'
   def registration_confirmation(user)
     @user = user
-    @url = "example@mail.com"
-    mail(to: @user.email,subject: "Welcome to my app")
+    @url = "http://example.com/log_in"
+    mail(to: @user.email, subject: "Welcome to my app")
   end
 
 end

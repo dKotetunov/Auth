bb5fa215339b2295360b8dbddcf0d588510e50df
dkotetunov <dkotetunov@gmail.com>
2014-12-18 20:19:40 +0200

    add userId to profiles

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,12 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20141218002424) do
+ActiveRecord::Schema.define(:version => 20141218181628) do
 
   create_table "profiles", :force => true do |t|
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.integer  "user_id"
   end
 
   create_table "users", :force => true do |t|

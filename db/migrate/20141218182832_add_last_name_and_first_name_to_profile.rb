a9b444eba44bcfee18a71350ca60b779f544bb23
dkotetunov <dkotetunov@gmail.com>
2014-12-18 20:34:17 +0200

    add last and first names to profiles

--- db/migrate/20141218182832_add_last_name_and_first_name_to_profile.rb
+++ db/migrate/20141218182832_add_last_name_and_first_name_to_profile.rb
@@ -1,7 +1,7 @@
 class AddLastNameAndFirstNameToProfile < ActiveRecord::Migration
   def change
 
-    add_column :profile, :first_name, :string
-    add_column :profile, :last_name, :string
+    add_column :profiles, :first_name, :string
+    add_column :profiles, :last_name, :string
   end
 end

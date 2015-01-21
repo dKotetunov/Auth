6752231a2e5f7124183c8592d6fe1997130768c7
dkotetunov <dkotetunov@gmail.com>
2015-01-21 18:26:42 +0200

    add modal window for edit admin

--- app/controllers/profiles_admins_controller.rb
+++ app/controllers/profiles_admins_controller.rb
@@ -17,9 +17,9 @@ class ProfilesAdminsController < ApplicationController
   def update
     @admin = Admin.find(params[:admin_id])
     @profile_admin = @admin.profile_admin
-    @profile_admin.update_attributes(params[:@profile_admin])
+    @profile_admin.update_attributes(params[:@profiles_admin])
     respond_to do |format|
-      if @profile_admin.update_attributes(params[:@profile_admin])
+      if @profile_admin.update_attributes(params[:@profiles_admin])
         format.js {}
       else
         format.html{render 'edit'}
